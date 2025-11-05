ebb58c5502f564a132153555cd6b0f662b28a5be
su080915 <tbr335@naver.com>
2025-11-05 22:52:43 +0900

    initial commit

---
+++ src/main/kotlin/com/team4/easybook/EasybookApplication.kt
@@ -0,0 +1,11 @@
+package com.team4.easybook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EasybookApplication
+
+fun main(args: Array<String>) {
+    runApplication<EasybookApplication>(*args)
+}
